d184dfa0e3898491f0df24fd0fb038d0266f84b5
bml03 <ganbolor0330@gmail.com>
2023-11-29 13:28:21 +0800

    rgb 6_11

--- RGB7011.js
+++ RGB7011.js
@@ -2,4 +2,19 @@ let seconds = 200;
 let sec = seconds % 60;
 let minWsec = seconds - sec;
 let min = minWsec / 60;
-console.log(min,sec);
+console.log(min, sec);
+
+function secs(num1) {
+  let sec = num1 % 60;
+  return sec;
+}
+function mins(num1) {
+  let sec;
+  sec = num1 % 60;
+  let minutWsec = num1 - sec;
+  let min = minutWsec / 60;
+  return min;
+}
+let mySecs = secs(200);
+let myMin = mins(200);
+console.log(myMin, mySecs);
