3aa3b97cd51f25083b5869d9819511df7dbb7328
Ганболор Мөнхдарь <23LP0102@pinecones-MacBook-Air-2.local>
2023-12-20 12:23:43 +0800

    index

---
+++ gogo/script.js
@@ -0,0 +1,5 @@
+const cardDiv = document.createElement("div");
+const cardBox = document.createElement("div");
+const cardButton = document.createElement("button");
+const cardHeader = document.createElement("h5");
+const cardText = document.createElement("p");
